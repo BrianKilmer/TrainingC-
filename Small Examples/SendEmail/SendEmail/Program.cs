572cbf1bb5a16bd179c9965bd7f8aae3df84765f
BrianKilmer <bkilmer@outreach.com>
2015-01-27 14:28:37 -0700

    update

--- Small Examples/SendEmail/SendEmail/Program.cs
+++ Small Examples/SendEmail/SendEmail/Program.cs
@@ -14,61 +14,69 @@ namespace SendEmail
     {
         static void Main(string[] args)
         {
-            //Create a new instance of the MailMessage Class
-            MailMessage mailMessage = new MailMessage();
-
-            //Create an array to store each email address
-            string[] emailTo = { "bkilmer@outreach.com","brianjkilmer@gmail.com" };
-
-            //Loop through the array and add them to the collection of recipients using the mailMessage.To.Add method
-            foreach (var m in emailTo)
-            {
-                mailMessage.To.Add(m);
-            }
+           List<string> emailList = new List<string>();
             
-            // set the 'subject',' body' and 'from' : (Addres and DisplayName - 2nd overload)
-            mailMessage.Subject = "HELLO";
-            mailMessage.Body = "THIs IS A TEST";
-            mailMessage.From = new MailAddress("brianjkilmer@gmail.com", "BK");
-
-            //Create a new instance of the class with (host and port) overlaod
-            SmtpClient smtpMail = new SmtpClient("smtp.gmail.com",587);
-
-            smtpMail.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtpMail.UseDefaultCredentials = true;
-            smtpMail.Credentials = new NetworkCredential("brianjkilmer@gmail.com", "*****");
-            //Set SSL to true
-            smtpMail.EnableSsl = true;
-
-            // this won't send unless i enable in my gmail account... and supply correct password **** above.
-            try
+            using (SqlConnection myConnection = new SqlConnection("data source=db-dev;initial catalog=print;integrated security=True;User ID=iusr_print_sq;Password=Dasesa809"))
             {
-                smtpMail.Send(mailMessage);
-                Console.WriteLine("Message Sent");
+                SqlCommand command = new SqlCommand("select email from employee where email = 'bkilmer@outreach.com'", myConnection);
+                myConnection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("email: {0}", reader.GetString(0));
+                        emailList.Add(reader.GetString(0).ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                
+                //---------------------------------------------
+
+                // Create a new instance of the MailMessage Class
+                MailMessage mailMessage = new MailMessage();
+
+                foreach (var emailTo in emailList)
+                {
+                    //Console.WriteLine("Email from list {0}", emailTo);
+                    //Add email to collection
+                    mailMessage.To.Add(emailTo);
+                }
+
+                // set the 'subject',' body' and 'from' : (Address and DisplayName - 2nd overload)
+                mailMessage.Subject = "HELLO";
+                mailMessage.Body = "THIs IS A TEST";
+                mailMessage.From = new MailAddress("brianjkilmer@gmail.com", "BK");
+
+                //Create a new instance of the SmtpClient class with (host and port) overlaod
+                SmtpClient smtpMail = new SmtpClient("192.168.131.45", 25);
+
+                smtpMail.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpMail.UseDefaultCredentials = true;
+                smtpMail.EnableSsl = false;
+
+                try
+                {
+                    smtpMail.Send(mailMessage);
+                    Console.WriteLine("Messages Sent");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+                Console.ReadLine();
+
+
+                
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message.ToString());
-            }
-            Console.ReadLine();
 
-            //fill from db
-            // using (SqlConnection cnn = new SqlConnection("server=(local);database=pubs;Integrated Security=SSPI"))  {
-            // SqlDataAdapter da = new SqlDataAdapter("select email from employee", cnn); 
-            // DataSet ds = new DataSet();
-            // da.Fill(ds, "employee");
 
-            // MailMessage mailMessage2 = new MailMessage();
 
-            // List<string> emailList = new List<string>();
-            // foreach(DataRow row in ds.Tables["employee"].Rows)
-            // {
-            //    // emailList.Add(row["name"].ToString());
-            //     //mailMessage.To.Add(row);
-
-            // }
-            //}
- 
 
         }
     }
