fe40cbd62303999df5e42717fec0f350dfe4e592
BrianKilmer <bkilmer@outreach.com>
2015-02-05 14:53:27 -0700

    added lovedare project for home

---
+++ thelovedarebook.com/source/Outreach.Web.TheLoveDareBook.MVCApplication/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Outreach.Web
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/scripts/prebody.js").Include(
+                        "~/scripts/google-analytics.js",
+                        "~/scripts/vwo.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts/postbody.js").Include(
+                        "~/scripts/jquery-{version}.js",
+                        "~/scripts/bootstrap.js",
+                        "~/scripts/site.js"));
+
+            bundles.Add(new StyleBundle("~/content/base.css").Include(
+                        "~/content/bootstrap.css",
+                        "~/content/site.css"));
+        }
+    }
+}
